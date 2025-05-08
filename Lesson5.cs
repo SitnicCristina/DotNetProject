ec68480a405b9d66ed7c513602b7e60f96071650
SitnicCristina <sitnic.cristina@gmail.com>
2025-05-08 23:11:06 +0200

    lesson 5

--- Lesson5.cs
+++ Lesson5.cs
@@ -1,10 +1,47 @@
-﻿namespace DotNetExamples
+﻿using System.Security.Claims;
+
+namespace DotNetExamples
 {
-    internal class Program
+    internal class Lesson5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Gestionarea datelor de naștere
+            /*
+             * Solicitați utilizatorului să introducă data sa de naștere(ziua, luna și anul) de la tastatură.
+            Utilizați tipul de date DateTime pentru a stoca această valoare.
+            Calculați vârsta utilizatorului pe baza datei de naștere și data curentă(utilizând metodele și proprietățile din clasa DateTime).
+            Afișați vârsta utilizatorului la ecran.
+            */
+            //Console.WriteLine("Enter the birth date: yyyy-mm-dd ");
+            //string birthDateInput = Console.ReadLine();
+            //DateTime.TryParse(birthDateInput, out DateTime birthDate);
+
+            //int userAge = DateTime.Now.Year - birthDate.Year;
+
+            //Console.WriteLine($"Age: {userAge}");
+            #endregion
+
+
+            #region Student
+            Console.WriteLine("Enter the student's details: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Speciality: ");
+            string speciality = Console.ReadLine();
+            Console.Write("Study Grade (1-6): ");
+            int studyGrade = int.Parse(Console.ReadLine());
+
+            var student = new Student(name,age,speciality,studyGrade);
+            student.DisplayStudentDetails();
+
+            Console.ReadLine();
+            #endregion
+
+
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetExamples
+{
+    /*
+     Definiți o structură pentru a reprezenta informațiile despre un student (nume, vârstă, specializare, anul de studii).
+     Solicitați utilizatorului să introducă informațiile despre un student de la tastatură.
+     Utilizați enumerațiile pentru a indica anul de studii. Valori admisibile: I, II, III, IV, V, VI.
+     Utilizați structura pentru a stoca aceste informații și pentru a le accesa ulterior.
+     Afișați informațiile despre student la ecran.
+            */
+    internal class Student
+    {
+        string name;
+        int age;
+        string speciality;
+        int studyGrade;
+
+        public Student(string name, int age, string speciality, int studyGrade)
+        {
+            this.name = name;
+            this.age = age;
+            this.speciality = speciality;
+            this.studyGrade = studyGrade;
+        }
+
+        enum YearGrade
+        {
+            I = 1,
+            II = 2,
+            III = 3,
+            IV = 4,
+            V = 5,
+            VI = 6
+        }
+
+        public void DisplayStudentDetails() 
+        {
+            if (Enum.IsDefined(typeof(YearGrade), studyGrade))
+            {
+                Console.WriteLine($"\nStudent Details:\n Name: {name} \n Age: {age} \n Speciality: {speciality} \n Study Grade: {(YearGrade)studyGrade}");
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid study grade.");
+            }
+        }
+
+
+
+    }
+}
