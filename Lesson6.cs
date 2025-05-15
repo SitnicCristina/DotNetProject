ba53ae0771dac31375fce2a9b794127c68aafd73
SitnicCristina <sitnic.cristina@gmail.com>
2025-05-15 23:44:49 +0200

    lesson 7

--- Lesson6.cs
+++ Lesson6.cs
@@ -8,108 +8,108 @@ namespace DotNetExamples
 {
     internal class Lesson6
     {
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+            //        #region Filtrarea și Procesarea Unor Obiecte Diverse
+            //        object[] obiecteMixte =
+            //        {
+            //           42,                                // întreg  
+            //           "Salut!",                          // șir de caractere  
+            //           new Student("Ioana", 21, "Biologie", 2) // obiect personalizat  
+            //        };
+
+            //        foreach (object obiectulMeu in obiecteMixte)
+            //        {
+            //            switch (obiectulMeu)
+            //            {
+            //                case int:
+            //                    if ((int)obiectulMeu % 2 == 0)
+            //                    {
+            //                        Console.WriteLine("Număr par: {0}", obiectulMeu);
+            //                    }
+            //                    else
+            //                    {
+            //                        Console.WriteLine("Număr impar: {0}", obiectulMeu);
+            //                    }
+            //                    break;
+            //                case string:
+            //                    if (obiectulMeu.ToString().Length > 5)
+            //                    {
+            //                        Console.WriteLine("Sir lung: {0}", obiectulMeu);
+            //                    }
+            //                    else
+            //                    {
+            //                        Console.WriteLine("Sir scurt: {0}", obiectulMeu);
+            //                    }
+            //                    break;
+            //                default:
+            //                    Console.WriteLine("Obiect necunoscut: {0}", obiectulMeu);
+            //                    break;
+            //            }
+            //        }
+            //        #endregion
+
+
+            //        #region Evaluarea calificativului studenților:
+            //        Console.WriteLine("Introdu nota ta de la test: ");
+            //        double nota = double.Parse(Console.ReadLine());
+
+            //        if (nota == 10)
+            //        {
+            //            Console.WriteLine("Excelent");
+            //        }
+            //        else if (nota is >= 8 and <= 9)
+            //        {
+            //            Console.WriteLine("Bun");
+            //        }
+            //        else if (nota is >= 5 and <= 7)
+            //        {
+            //            Console.WriteLine("Suficient");
+            //        }
+            //        else if (nota is >= 1 and <= 4)
+            //        {
+            //            Console.WriteLine("Necalificat");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Introdu o nota intre 10 si 1");
+            //        }
+
+            //        #endregion Construiți un program simplu de gestionare a meniului unui restaurant. 
+
+
+            //        #region Construiți un program simplu de gestionare a meniului unui restaurant. 
+            //        Console.WriteLine("Introduceti optiune din meniu: ");
+            //        int nrOrdineOptiuneMeniu = int.Parse(Console.ReadLine());
+
+            //        switch (nrOrdineOptiuneMeniu)   
+            //        {
+            //            case 1:
+            //                Console.WriteLine((Meniu)nrOrdineOptiuneMeniu);
+            //                break;
+            //            case 2:
+            //                Console.WriteLine((Meniu)nrOrdineOptiuneMeniu);
+            //                break;
+            //            case 3:
+            //                Console.WriteLine((Meniu)nrOrdineOptiuneMeniu);
+            //                break;
+            //            case 4:
+            //                Console.WriteLine((Meniu)nrOrdineOptiuneMeniu);
+            //                break;
+            //            default:
+            //                Console.WriteLine("Introduceti maximum optiunea 4");
+            //                break;
+            //        }
+            //        #endregion
+        //}
+
+    }
+
+        enum Meniu
         {
-            #region Filtrarea și Procesarea Unor Obiecte Diverse
-            object[] obiecteMixte =
-            {
-               42,                                // întreg  
-               "Salut!",                          // șir de caractere  
-               new Student("Ioana", 21, "Biologie", 2) // obiect personalizat  
-            };
-
-            foreach (object obiectulMeu in obiecteMixte)
-            {
-                switch (obiectulMeu)
-                {
-                    case int:
-                        if ((int)obiectulMeu % 2 == 0)
-                        {
-                            Console.WriteLine("Număr par: {0}", obiectulMeu);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Număr impar: {0}", obiectulMeu);
-                        }
-                        break;
-                    case string:
-                        if (obiectulMeu.ToString().Length > 5)
-                        {
-                            Console.WriteLine("Sir lung: {0}", obiectulMeu);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sir scurt: {0}", obiectulMeu);
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Obiect necunoscut: {0}", obiectulMeu);
-                        break;
-                }
-            }
-            #endregion
-
-
-            #region Evaluarea calificativului studenților:
-            Console.WriteLine("Introdu nota ta de la test: ");
-            double nota = double.Parse(Console.ReadLine());
-
-            if (nota == 10)
-            {
-                Console.WriteLine("Excelent");
-            }
-            else if (nota is >= 8 and <= 9)
-            {
-                Console.WriteLine("Bun");
-            }
-            else if (nota is >= 5 and <= 7)
-            {
-                Console.WriteLine("Suficient");
-            }
-            else if (nota is >= 1 and <= 4)
-            {
-                Console.WriteLine("Necalificat");
-            }
-            else
-            {
-                Console.WriteLine("Introdu o nota intre 10 si 1");
-            }
-
-            #endregion Construiți un program simplu de gestionare a meniului unui restaurant. 
-
-
-            #region Construiți un program simplu de gestionare a meniului unui restaurant. 
-            Console.WriteLine("Introduceti optiune din meniu: ");
-            int nrOrdineOptiuneMeniu = int.Parse(Console.ReadLine());
-
-            switch (nrOrdineOptiuneMeniu)   
-            {
-                case 1:
-                    Console.WriteLine((Meniu)nrOrdineOptiuneMeniu);
-                    break;
-                case 2:
-                    Console.WriteLine((Meniu)nrOrdineOptiuneMeniu);
-                    break;
-                case 3:
-                    Console.WriteLine((Meniu)nrOrdineOptiuneMeniu);
-                    break;
-                case 4:
-                    Console.WriteLine((Meniu)nrOrdineOptiuneMeniu);
-                    break;
-                default:
-                    Console.WriteLine("Introduceti maximum optiunea 4");
-                    break;
-            }
-            #endregion
+            Supa = 1,
+            SalataCaesar = 2,
+            SalataGreceasca =3,
+            Inghetata = 4
         }
-
-    }
-
-    enum Meniu
-    {
-        Supa = 1,
-        SalataCaesar = 2,
-        SalataGreceasca =3,
-        Inghetata = 4
-    }
 }
---
+++ Lesson7.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetExamples
+{
+    internal class Lesson7
+    {
+        static void Main(string[] args)
+        {
+
+            #region
+            //Console.WriteLine("Introduceti numarul de studenti: ");
+            //int nrStudenti = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < nrStudenti; i++)
+            //{
+            //    Console.WriteLine("Introduceti notele elevului {0} prin spatiu: ", i + 1);
+            //    string note = Console.ReadLine();
+
+            //    var splitArray = note.Split(" ");
+            //    int[] array = Array.ConvertAll(splitArray, int.Parse);
+
+            //    Console.WriteLine($"Media notelor {array.Average()}\n");
+            //}
+            #endregion
+
+            #region
+            DateAngajati[] angajati = new DateAngajati[3];
+            double sum = 0;
+
+            for (int i = 0; i < angajati.Length; i++)
+            { 
+                Console.WriteLine($"Introduceti datele la angajatul : {i + 1}: ");
+                Console.Write("Nme: ");
+                angajati[i].name = Console.ReadLine();
+
+                Console.Write("Departament: ");
+                angajati[i].depart = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                angajati[i].salary = double.Parse(Console.ReadLine());
+
+                sum += angajati[i].salary;
+                
+            }
+
+            Console.WriteLine("\nMedia salariului este {0} ", sum / 3);
+            #endregion
+
+        }
+    }
+
+    struct DateAngajati
+    {
+        public string name;
+        public string depart;
+        public double salary;
+    }
+}
